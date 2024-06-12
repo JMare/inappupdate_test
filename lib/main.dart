9a4d74d3a6b9ec7fea35f79eb57ad460b0604193
James Mare <jamesmare12@gmail.com>
2024-06-12 11:47:55 +1000

    first test

--- lib/main.dart
+++ lib/main.dart
@@ -59,7 +59,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Releases'),
+        title: Text('Releases (v1.0)'),
       ),
       body: FutureBuilder<List<Release>>(
         future: futureReleases,
